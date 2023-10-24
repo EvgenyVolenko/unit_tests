02a38351567c78df3177945d5f7fe25893fe6529
Evgeny <evolenko@inbox.ru>
2023-10-25 01:49:21 +0300

    Added Seminar 5 Task_2-5

--- unit-tests/Test/seminars/five/MainTest.java
+++ unit-tests/Test/seminars/five/MainTest.java
@@ -2,18 +2,28 @@ package seminars.five;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import seminars.five.number.MaxNumberModule;
 import seminars.five.number.RandomNumberModule;
+import seminars.five.order.OrderService;
+import seminars.five.order.PaymentService;
+import seminars.five.user.UserRepository;
+import seminars.five.user.UserService;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 
 class MainTest {
 
+    //    Создайте два модуля. Первый модуль генерирует список случайных чисел. Второй модуль находит
+//    максимальное число в этом списке.
+//    Вам нужно сначала написать юнит-тесты для каждого модуля, затем написать интеграционный
+//    тест, который проверяет, что оба модуля работают вместе правильно
     List<Integer> randomList;
 
     @BeforeEach
@@ -52,4 +62,34 @@ class MainTest {
 
         assertThat(Collections.max(randomList)).isEqualTo(maxValue);
     }
+
+//    У вас есть два класса - UserService и UserRepository. UserService использует UserRepository для
+//    получения информации о пользователе. Ваша задача - написать интеграционный тест, который
+//    проверяет, что UserService и UserRepository работают вместе должным образом.
+
+    @Test
+    void integrationUserTest() {
+        UserRepository userRepository = new UserRepository();
+        UserService userService = new UserService(userRepository);
+
+        String result = userService.getUserName(5);
+
+        assertThat(result).isEqualTo("User 5");
+    }
+
+//    У вас есть сервис по обработке заказов, состоящий из двух классов: OrderService и PaymentService.
+//    Класс OrderService обрабатывает заказы и делает вызовы к PaymentService для обработки платежей.
+//    Ваша задача - написать интеграционный тест, который проверяет,
+//    что OrderService и PaymentService взаимодействуют корректно
+
+    @Test
+    void orderServiceTest() {
+        PaymentService paymentService = new PaymentService();
+        OrderService orderService = new OrderService(paymentService);
+
+        boolean check = orderService.placeOrder(Mockito.anyString(), 123.45);
+
+        assertTrue(check);
+    }
 }
+
